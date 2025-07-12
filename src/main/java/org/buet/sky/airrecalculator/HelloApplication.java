8d76bad60f2af444018f0c86d89f678133a30fa3
Arko07-quanta <chowdhuryarko084@gmail.com>
2025-07-12 15:34:24 +0600

    done shit

--- src/main/java/org/buet/sky/airrecalculator/HelloApplication.java
+++ src/main/java/org/buet/sky/airrecalculator/HelloApplication.java
@@ -10,10 +10,11 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("MainPage.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("sex!");
         stage.setScene(scene);
+        stage.centerOnScreen();
         stage.show();
     }
 
---
+++ src/main/java/org/buet/sky/airrecalculator/MainPage.java
@@ -0,0 +1,26 @@
+package org.buet.sky.airrecalculator;
+
+import com.almasb.fxgl.ui.FXGLButton;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainPage {
+    private Stage stage;
+
+
+    MainPage(Stage stage){
+        this.stage = stage;
+    }
+
+    public void show() throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("MainPage.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        stage.centerOnScreen();
+    }
+}
