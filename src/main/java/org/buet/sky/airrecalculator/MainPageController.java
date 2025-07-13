cfb96a372934231de7ea0664c4a08e744ea9665e
Arko07-quanta <chowdhuryarko084@gmail.com>
2025-07-13 10:21:07 +0600

    link reg page with main

--- src/main/java/org/buet/sky/airrecalculator/MainPageController.java
+++ src/main/java/org/buet/sky/airrecalculator/MainPageController.java
@@ -1,5 +1,20 @@
 package org.buet.sky.airrecalculator;
 
-public class MainPageController {
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
+import javafx.event.ActionEvent;
+import java.io.IOException;
+
+public class MainPageController {
+    @FXML
+    public void onRegister(ActionEvent event) throws IOException {
+        Stage stage = ((Stage) ((Node) event.getSource()).getScene().getWindow());
+        RegistrationPage registrationPage = new RegistrationPage(stage);
+        registrationPage.show();
+    }
 }
--- src/main/java/org/buet/sky/airrecalculator/RegistrationPageController.java
+++ src/main/java/org/buet/sky/airrecalculator/RegistrationPageController.java
@@ -1,4 +1,24 @@
 package org.buet.sky.airrecalculator;
 
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import javafx.event.ActionEvent;
+import java.io.IOException;
+
+
 public class RegistrationPageController {
+    @FXML
+    public void onCreateAccount(ActionEvent event) throws IOException {
+        return;
+    }
+
+    @FXML
+    public void onMain(ActionEvent event) throws IOException {
+        Stage stage = ((Stage) ((Node) event.getSource()).getScene().getWindow());
+        MainPage mainPage = new MainPage(stage);
+        mainPage.show();
+    }
 }
