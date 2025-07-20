778c35921fd2f8ed93a40f3ab4b1fbb47e035f6f
Arko07-quanta <chowdhuryarko084@gmail.com>
2025-07-20 20:41:34 +0600

    done something

--- src/main/java/org/buet/sky/airrecalculator/Main.java
+++ src/main/java/org/buet/sky/airrecalculator/Main.java
@@ -13,25 +13,25 @@ import java.io.IOException;
 
 
 
-public class Main extends Application {
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("CityMap.fxml"));
-        primaryStage.setScene(new Scene(root));
-        primaryStage.setTitle("City Mapper");
-        primaryStage.show();
-    }
-
-    public static void main(String[] args) {
-        launch(args);
-    }
-}
-
-
+//public class Main extends Application {
+//    @Override
+//    public void start(Stage primaryStage) throws Exception {
+//        Parent root = FXMLLoader.load(getClass().getResource("CityMap.fxml"));
+//        primaryStage.setScene(new Scene(root));
+//        primaryStage.setTitle("City Mapper");
+//        primaryStage.show();
+//    }
+//
+//    public static void main(String[] args) {
+//        launch(args);
+//    }
+//}
 
 
 
-/*
+
+
+
 
 
 
@@ -53,4 +53,3 @@ public class Main extends Application {
         launch(args);
     }
 }
-*/
